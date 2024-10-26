6ac9d6186fbf7e10e9275bd24b662067ae09d810
Hamish MacDonald <hamish.macdonald@neds.com>
2024-10-27 09:16:29 +1000

    casting shorts to char to see the behaviour

---
+++ char_star_tricks.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+  // double x = 3.1416;
+  // char x = 'A';
+  short x = 256;
+
+  int ct = 127;
+  for (int i = 0; i < ct; i++) {
+    x = 256 + i;
+
+    char ch = *(char *)&x;
+    // printf("Size of x: %ld\n", sizeof(x));
+    // printf("Size of ch: %ld\n", sizeof(ch));
+    printf("x in decimal: %d\n", x);
+    printf("x in hexadecimal: %x\n", x);
+    if (ch == 0x0b) {
+      printf("ch as a character: \\x0b (vertical tab)\n");
+    } else if (ch == 0x1b) {
+      printf("ch as a character: \\x1b (escape)\n");
+    } else {
+      printf("ch as a character: %c\n", ch);
+    }
+    printf("ch as an int: %d\n\n", ch);
+  }
+}
