28758fc79bac620578aa735cbde6ce521df4a0ab
Hamish MacDonald <hamish.macdonald@neds.com>
2024-11-01 10:17:04 +1000

    void* example

---
+++ void_star_example_memcpy.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void) {
+  char s[] = "Goats!";
+  char t[100];
+
+  memcpy(t, s, 7); // Copy 7 bytes--including the NUL terminator!
+
+  printf("%s\n", t); // "Goats!"
+
+  int a[] = {11, 22, 33};
+  int b[3];
+
+  memcpy(b, a, 3 * sizeof(int)); // Copy 3 ints of data
+
+  printf("%d\n", b[1]); // 22
+}
